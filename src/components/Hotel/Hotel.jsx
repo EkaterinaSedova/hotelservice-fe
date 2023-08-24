2b46d22a9fa801114bf461373a14944153c85601
Екатерина Седова <katena.sedova@gmail.com>
2023-08-24 10:29:17 +0300

    пофиксила

--- src/components/Hotel/Hotel.jsx
+++ src/components/Hotel/Hotel.jsx
@@ -3,31 +3,32 @@ import {useDispatch, useSelector} from "react-redux";
 import {getHotelById} from "../../store/hotels/hotelsSlice";
 import Header from "../Header/Header";
 import styles from './Hotel.module.css'
+import {useParams} from "react-router-dom";
 
 const Hotel = () => {
-    const dispatch = useDispatch();
+    const {hotel} = useSelector(({hotels}) => hotels);
     const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+    const {id} = useParams();
 
     useEffect(() => {
-        dispatch(getHotelById(3));
-        setLoading(false);
-    }, [dispatch])
-
-    const {hotel} = useSelector(({hotels}) => hotels);
+        (async () => {
+            await dispatch(getHotelById(id));
+            setLoading(false);
+        })()
+    }, [id, dispatch])
 
     return (
         <>
             <Header/>
-        { loading ?
-                <div>
-                    loading...
-                </div>
+            {loading ?
+                <div>loading...</div>
                 :
                 <div>
-                <div className={styles.hotel}>
-                    <div>
-                        <img className={styles.hotelImg} src={'http://localhost:80/' + hotel.images[0]} alt={'hotelImage'}/>
-                    </div>
+                    <div className={styles.hotel}>
+                        <div>
+                            <img className={styles.hotelImg} src={'http://localhost:80/' + hotel.images[0]} alt={'hotelImage'}/>
+                        </div>
                         <div className={styles.hotelInfo}>
                             <span className={styles.hotelName}>{hotel.name}</span>
                             <span>{hotel.description}</span>
@@ -35,7 +36,7 @@ const Hotel = () => {
                         </div>
                     </div>
                 </div>
-        }
+            }
         </>
     );
 };
